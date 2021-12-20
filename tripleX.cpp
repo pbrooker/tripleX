451d72a0faa2be34dac84f42d94c250f36e3baeb
Paul Brooker <pdbrooker@gmail.com>
2021-12-20 14:46:55 -0400

    continue with tutorial

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
 	// Print welcome messages to start game
-	std::cout << "You are a secret agent breaking into a secure server room";
-	std::cout << std::endl;
-	std::cout << "Enter the correct code to continue...";
+	std::cout << "\n\n<<<<<<<<<<<<<<<<<<<<|[|]|>>>>>>>>>>>>>>>>>>>>\n";
+	std::cout << "You are a secret agent breaking into a secure server room\n";
+	std::cout << "Enter the correct code to continue...\n\n";
+	std::cout << "-------\n";
+	std::cout << "|1 2 3|\n";
+	std::cout << "|4 5 6|\n";
+	std::cout << "|7 8 9|\n";
+	std::cout << "|* 0 #|\n";
+	std::cout << "-------\n\n";
+}
+
+void PlayGame()
+{
+	PrintIntroduction();
 
 	// Declare our code
 	const int CodeA = 4;
@@ -17,13 +28,14 @@ int main()
 
 	// Print results
 	std::cout << std::endl;
-	std::cout << "There are 3 numbers in the code" << std::endl;
-	std::cout << "The codes add up to a total of: " << CodeSum << std::endl;
-	std::cout << "The numbers multiply to a total of: " << CodeProduct << std::endl;
+	std::cout << "There are 3 numbers in the code";
+	std::cout << "\nThe codes add up to a total of: " << CodeSum;
+	std::cout << "\nThe numbers multiply to a total of: " << CodeProduct;
+	std::cout << std::endl << std::endl;
 
 	// store player guess
 	int GuessA, GuessB, GuessC;
-	std::cin >> GuessA >> std::cin >> GuessB >> std::cin >> GuessC;
+	std::cin >> GuessA >> GuessB >> GuessC;
 
 	// validate player guess
 	int GuessSum = GuessA + GuessB + GuessC;
@@ -32,10 +44,16 @@ int main()
 	// print guess results
 	if (GuessSum == CodeSum && GuessProduct == CodeProduct)
 	{
-		std::cout << "You found the right code! You enter the server room. You're a winner!";
-	} else {
-		std::cout << "Immediately upon entering the incorrect code, the hallway is exposed to searing flames. You have died a horrible and painful death.";
+		std::cout << "\n\nYou found the right code! You enter the server room. You're a winner!\n";
 	}
+	else {
+		std::cout << "\n\nImmediately upon entering the incorrect code, the hallway is exposed to searing flames. You have died a horrible and painful death.\n";
+	}
+}
+
+int main()
+{
+	PlayGame();
 
 	return 0;
 }
